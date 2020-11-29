73de850169dac068a7a2a2f62789cd946b25996e
Xavier Vila Rubio <xvrvilarubio2001@gmail.com>
2020-11-29 14:08:25 +0100

    Finished 1.Checking information into the Pokedex

--- src/ash-p1.c
+++ src/ash-p1.c
@@ -8,77 +8,94 @@
 #include "packet.h"
 #include "pokemon.h"
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+#define KYEL "\x1B[33m"
+#define KBLU "\x1B[34m"
+#define KMAG "\x1B[35m"
+#define KCYN "\x1B[36m"
+#define KWHT "\x1B[37m"
 
 char *args2[] = {"pokedex", "pokedex", NULL};
 
 #define TRUE 1
 #define FALSE 0
-enum State {WaitingPokedex=1, WaitingPokemon=2, Fighting=3, EndFight=4}; 
+enum State
+{
+    WaitingPokedex = 1,
+    WaitingPokemon = 2,
+    Fighting = 3,
+    EndFight = 4
+};
 enum State currentStatus;
 
-char msg[100];
-void cont(){}
+pid_t child_pid;
+int status;
+int fd[2], fd2[2];
+
+void cont() {}
+
+void end(){
+    close(fd[1]);
+    close(fd2[0]);
+    kill(child_pid, SIGUSR1);
+    wait(&status);
+    exit(0);
+}
 
 int main(int arc, char *arv[])
 {
-    signal(SIGCONT, cont);
-    int status;
-    int fd[2], fd2[2];
-    int pokemonId;
+    //Preparem el tractament de senyals
+    signal(SIGUSR1, cont);
+    signal(SIGINT, end);
 
+    //Creem les pipes
     pipe(fd);
-    if(fork()==0){
+    pipe(fd2);
+    //Guardem el pid del fill per a poder-lo matar
+    child_pid = fork();
+    if (child_pid == 0)
+    {
         close(0);
         dup(fd[0]);
+        close(1);
+        dup(fd2[1]);
         close(fd[0]);
         close(fd[1]);
+        close(fd2[0]);
+        close(fd2[1]);
+        //Executem la pokedex
         execv(args2[0], args2);
         exit(0);
     }
-
+    //Esperem a que la pokedex carregui
     pause();
 
-    //@Jordi: El pare no cal que tanqui la sortida 1, pot fer servir directament el pipe fd[1]
-    //close(1);
-    //dup(fd[1]);
-
+    //Tanquem descriptors innecessaris
     close(fd[0]);
-    //@Jordi: La neteja de la pipe s'ha de fer quan ja no tinguis que fer-la servir.
-    //close(fd[1]);
+    close(fd2[1]);
 
-    //while(1){
+    while (1)
+    {
+        //Resetejem la variable per a que entri al while
+        int pokemonId = 0;
+        while (pokemonId < 1 || pokemonId > 151)
+        {
+            printf("\nEnter a pokemonId between (1-151): ");
+            scanf("%d", &pokemonId);
+        }
 
-    printf("Enter a pokemonId:  ");
-    scanf("%d", &pokemonId);
+        //Li passem el pokemonId introduit a la pokedex per la pipe(fd)
+        write(fd[1], &pokemonId, sizeof(int));
 
-    //@Jordi: El pare en aquest punt pot fer anar directament la pipe
-    //@Jordi: Recorda que el write necessita l'@ -> &pokemonId en lloc de pokemonId declarat com int pokemonId.
-
-    //@Jordi: Si li passes així millor i més simple fer un read a pokedex.c.
-    //@Jordi: Millor sempre fer anar write i read amb pipes.
-    write(fd[1], &pokemonId, sizeof(int));
-
-    //@Jordi: Si vols fer anar scanf a pokedex.c has de passar-li així si no et donarà problemes.
-    //char pokemonIdstr[30];
-    //sprintf(pokemonIdstr,"%d",pokemonId);
-    //write(fd[1],&pokemonIdstr,sizeof(pokemonIdstr));
-
-    // @Jordi: El pare no cal tancar la stdout (1).
-    //close(1);
-    //}
-
-    wait(&status);
-
-    //@Jordi: Aqui el pare ja no escriura més a la pipe ja que el fill ha acabat, per tant es pot tancar.
-    close(fd[1]);
+        struct pokemon pkm;
+        //Llegim l'apuntador de l'estructura que ens ha passat la pokedex per la pipe(fd2)
+        read(fd2[0], &pkm, sizeof(struct pokemon));
+        //Prints per comprovar que hem rebut l'apuntador de manera correcta
+        printf("Pokemon name: %s\n", pkm.name);
+        printf("Pokemon hp: %d\n", pkm.hp);
+    }
 
     exit(0);
 }
--- src/pokedex.c
+++ src/pokedex.c
@@ -1,8 +1,8 @@
-#include <stdio.h>       /* fopen, fprintf, fread, fscanf, fseek, ftell, fclose */
-#include <string.h>      /* strcmp, strncpy */
-#include <stdlib.h>      /* malloc, free, exit, atoi */
-#include <unistd.h>      /* sleep, unlink, getpid */
-#include <sys/wait.h>    /* wait */
+#include <stdio.h>	  /* fopen, fprintf, fread, fscanf, fseek, ftell, fclose */
+#include <string.h>	  /* strcmp, strncpy */
+#include <stdlib.h>	  /* malloc, free, exit, atoi */
+#include <unistd.h>	  /* sleep, unlink, getpid */
+#include <sys/wait.h> /* wait */
 #include <signal.h>
 #include <errno.h>
 #include "pokemon.h"
@@ -10,71 +10,77 @@
 
 struct pokemon pokedex[151];
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
+	FILE *f = fopen("pokedex.csv", "r");
 
-  FILE* f = fopen("pokedex.csv", "r");
+	if (!f)
+	{
+		perror("fopen");
+		exit(1);
+	}
 
-  if(!f) {
-    perror("fopen");
-    exit(1);
-  }
+	char *buf = malloc(151);
+	char *info;
+	int i = 0;
 
-  char *buf = malloc(151);
-  char *info;
-  int i = 0;
+	while (fgets(buf, 151, f) != NULL)
+	{
 
-  while (fgets(buf, 151, f) != NULL) {
+		if ((strlen(buf) > 0) && (buf[strlen(buf) - 1] == '\n'))
+			buf[strlen(buf) - 1] = '\0';
 
-    if ((strlen(buf)>0) && (buf[strlen (buf) - 1] == '\n'))
-        buf[strlen (buf) - 1] = '\0';
+		struct pokemon p;
 
-    struct pokemon p;
+		info = strtok(buf, ",");
+		int id = atoi(info);
+		p.id = id;
+		info = strtok(NULL, ",");
+		strcpy(p.name, info);
+		info = strtok(NULL, ",");
+		strcpy(p.type[0], info);
+		info = strtok(NULL, ",");
+		strcpy(p.type[1], info);
+		info = strtok(NULL, ",");
+		p.total = atoi(info);
+		info = strtok(NULL, ",");
+		p.hp = atoi(info);
+		info = strtok(NULL, ",");
+		p.attack = atoi(info);
+		info = strtok(NULL, ",");
+		p.defense = atoi(info);
+		info = strtok(NULL, ",");
+		p.spAttack = atoi(info);
+		info = strtok(NULL, ",");
+		p.spDefense = atoi(info);
+		info = strtok(NULL, ",");
+		p.speed = atoi(info);
+		info = strtok(NULL, ",");
+		p.generation = atoi(info);
+		info = strtok(NULL, ",");
+		p.legendary = atoi(info);
+		pokedex[i] = p;
+		i++;
+	}
 
-    info = strtok(buf, ",");
-    int id  = atoi(info);
-    p.id = id;
-    info = strtok(NULL, ","); strcpy(p.name, info);
-    info = strtok(NULL, ","); strcpy(p.type[0], info);
-    info = strtok(NULL, ","); strcpy(p.type[1], info);
-    info = strtok(NULL, ","); p.total = atoi(info);
-    info = strtok(NULL, ","); p.hp = atoi(info); 
-    info = strtok(NULL, ","); p.attack = atoi(info);
-    info = strtok(NULL, ","); p.defense = atoi(info);
-    info = strtok(NULL, ","); p.spAttack = atoi(info);
-    info = strtok(NULL, ","); p.spDefense = atoi(info);
-    info = strtok(NULL, ","); p.speed = atoi(info);
-    info = strtok(NULL, ","); p.generation = atoi(info);
-    info = strtok(NULL, ","); p.legendary = atoi(info);
-    pokedex[i]=p;
-    i++;
-  }
-  
-  fclose(f);
+	fclose(f);
 
-  // @Jordi: Jo no faria anar SIGCONT en aquest cas millor, SIGUSR1 o SIGUSR2
-  // Ja que, si no ho demano SIGCONT ja té les seves funcions per defecte.
-  kill(getppid(), SIGCONT);
+	//Avisem a l'Ash de que ja la pokedex ja ha carregat
+	kill(getppid(), SIGUSR1);
 
-  //while(1){
-  int pokemonId;
+	while (1)
+	{
+		int pokemonId;
 
-    // @Bug: El problema es que pokemonid tenia un nombre més gran que 151 i al buscar al array segmenation fault.
-    // scanf("%d", &pokemonId);
+		//Llegim el pokemonId que ens passen per la pipe(fd)
+		read(0, &pokemonId, sizeof(int));
 
-    //@Jordi: Una solució utiltizar el read, ja que l'scanf té un comportament diferent.
-    //        Mira el pare, on t'he deixat les línies comentades per que et funcioni amb el scanf també.
-    read(0,&pokemonId,sizeof(int));
+		struct pokemon p;
+		p = pokedex[pokemonId - 1];
 
-    //@Jordi: Amb aquest xivato pot comprovar el que li envies si actives el scanf, per veure el problema que et comento.
-    printf("he rebut... %d\n",pokemonId);fflush(stdout);
+		//Retornem l'apuntador del pokemon sol·licitat per la pipe(fd2)
+		write(1, &p, sizeof(struct pokemon));
+	}
 
-    struct pokemon p;
-    p = pokedex[pokemonId - 1];
-
-    // @Jordi: Si fas anar printf, assegura't de fer stdout al final, si no pot ser que no observis el resultat.
-    printf("The pokemon with id:%d it is known as: %s.\n", pokemonId, p.name);
-    fflush(stdout);
- //}
-
-  exit(0);
+	exit(0);
 }
